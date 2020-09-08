4247a3580b5ade1712c9554f7b048cea54b92c35
fuku1988 <momo.c.1988333@gmail.com>
2020-09-09 02:09:49 +0900

    room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
    # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+
+  has_many :room_users
+  has_many :messages
 
 end
